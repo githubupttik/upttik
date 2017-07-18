66e7d650aec23634036a49719a3e88a17f542058
Puskom <puskom@puskom-asek>
2017-07-18 11:02:46 +0700

    ganti logo

--- templates/jd_newjersey/index.php
+++ templates/jd_newjersey/index.php
@@ -83,9 +83,9 @@ $templateName = $app->getTemplate();
     <header id="rt-top-surround">
 		<?php if ($gantry->countModules('top')) : ?>
 		<?php if($positionTopbg != 'transparent') { ?>
-		<img src="templates/jd_newjersey/logo.png"/><div class="wow fadeIn" id="rt-top" <?php echo $gantry->displayClassesByTag('rt-top');?> style="background:<?php echo $positionTopbg;?> no-repeat center top; background-size:cover;">
+		<div class="wow fadeIn" id="rt-top" <?php echo $gantry->displayClassesByTag('rt-top');?> style="background:<?php echo $positionTopbg;?> no-repeat center top; background-size:cover;">
 		<?php } else { ?>
-		<div class="wow fadeIn" id="rt-top" <?php echo $gantry->displayClassesByTag('rt-top');?>>
+		<img src="templates/jd_newjersey/logo.png"/><div class="wow fadeIn" id="rt-top" <?php echo $gantry->displayClassesByTag('rt-top');?>>
 		<?php } ?>
 			<div class="rt-container">
 				<?php echo $gantry->displayModules('top','standard','standard'); ?>
